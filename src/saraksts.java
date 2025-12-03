bfdbf369d15bafd198ab8baedc23675e807a683b
Gansons <gansonsraivis@gmail.com>
2025-12-03 08:13:43 +0200

    Pabeigta programma

--- src/saraksts.java
+++ src/saraksts.java
@@ -79,9 +79,77 @@ public class saraksts {
 						"Produkts atrasts "+saraksts.indexOf(koAtrast)+". pozīcijā"
 						: "Produkts netika atrasts sarakstā!", "Infromācija", JOptionPane.INFORMATION_MESSAGE);
 				break;
+				
+				
+			case "5":
+				
+				do {
+					koPievienot = 
+							JOptionPane.showInputDialog("Kādu produktu pievienot?");
+					kurPievienot = 
+							Integer.parseInt(JOptionPane.showInputDialog("Kurā pozīcijā pievienot?"));
+				}while((jauEksiste(saraksts, koPievienot) == true) || !koPievienot.matches("^[\\p{L} ]+$") || 
+						saraksts.size() <= kurPievienot || kurPievienot < 0);
+				saraksts.add(kurPievienot, koPievienot);
+				break;
+				
+			case "6":
+				do {
+					koNonemt = JOptionPane.showInputDialog("Kuru produktu noņemt?");
+				}while(!koNonemt.matches("^[\\p{L} ]+$") || !jauEksiste(saraksts, koNonemt));
+				JOptionPane.showMessageDialog(null, "Produkts noņemts!", "Paziņojums", JOptionPane.INFORMATION_MESSAGE);
+				break;
+				
+				
+			case "7":
+				kurNonemt = Integer.parseInt(JOptionPane.showInputDialog("Ievadi indeksa vietu kuru nonemt (no 0 lidz " + (saraksts.size()-1) + "):"));
+				if(kurNonemt <0 || kurNonemt > saraksts.size()-1) {
+					JOptionPane.showMessageDialog(null, "Nepareiza indeksa vieta!", "Kļūda", JOptionPane.ERROR_MESSAGE);
+				}else {
+					saraksts.remove(kurNonemt);
+					JOptionPane.showMessageDialog(null, "Produkts veiksmigi noņemts no saraksta!", "Informacija", JOptionPane.INFORMATION_MESSAGE);
+				}
+				break;
+			case "8":
+				if(!saraksts.isEmpty()) {
+					saraksts.removeFirst();
+					JOptionPane.showMessageDialog(null, "Pirmais produkts veiksmigi noņemts no saraksta!", "Informacija", JOptionPane.INFORMATION_MESSAGE);
+				}else {
+					JOptionPane.showMessageDialog(null, "Saraksts ir tukss!", "Kļūda", JOptionPane.ERROR_MESSAGE);
+				}
+				break;
+				case "9":
+					if(!saraksts.isEmpty()) {
+						saraksts.removeLast();
+						JOptionPane.showMessageDialog(null, "Pedejais produkts veiksmigi noņemts no saraksta!", "Informacija", JOptionPane.INFORMATION_MESSAGE);
+					}else {
+						JOptionPane.showMessageDialog(null, "Saraksts ir tukss!", "Kļūda", JOptionPane.ERROR_MESSAGE);
+					}
+					break;
+					case "10":
+						do {
+							kuruMainit = Integer.parseInt(JOptionPane.showInputDialog("Ievadi indeksa vietu kuru mainit (no 0 lidz " + (saraksts.size()-1) + "):"));
+							arKoAizstāt = JOptionPane.showInputDialog("Ievadi produktu ar ko aizstatit:");
+							if(kuruMainit <0 || kuruMainit > saraksts.size()-1) {
+								JOptionPane.showMessageDialog(null, "Nepareiza indeksa vieta!", "Kļūda", JOptionPane.ERROR_MESSAGE);
+								}else {
+									saraksts.set(kuruMainit, arKoAizstāt.toLowerCase());
+									JOptionPane.showMessageDialog(null, "Produkts veiksmigi aizstatits!", "Informacija", JOptionPane.INFORMATION_MESSAGE);
+								}
+							
+						}while(!arKoAizstāt.matches("^[\\p{L} ]+$"));
+						break;
+						case "11":
+							saraksts.sort(String.CASE_INSENSITIVE_ORDER);
+							JOptionPane.showMessageDialog(null, "Saraksts veiksmigi sakartots aflabetiski!", "Informacija", JOptionPane.INFORMATION_MESSAGE);
+							break;
+							case "12":
+								saraksts.clear();
+								JOptionPane.showMessageDialog(null, "Saraksts veiksmigi nodzests!", "Informacija", JOptionPane.INFORMATION_MESSAGE);
+								break;
 			}
 			
-			
+		
 			
 		}while(!izvele.equals("0"));
 
